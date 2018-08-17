14a17660ac6c38bf9b08adad7f283245eda7f4a1
xiny <yao_xinyu@outlook.com>
2018-08-17 21:50:09 +0800

    修改 搜索框 type=text => type=search

--- Application/Admin/Controller/TestController.class.php
+++ Application/Admin/Controller/TestController.class.php
@@ -3,8 +3,8 @@
  * Created by Makecode
  * User: Xiny https://xbug.top i@xiny9.com
  * Coding Standard: PSR2
- * Date: 2018-07-24
- * Time: 23:08
+ * Date: 2018-08-17
+ * Time: 21:39
  */
 
 namespace Admin\Controller;
@@ -23,6 +23,6 @@ class TestController extends AdminBaseController
         $this->addTemplet = 'Test/add';
         $this->editTemplet = 'Test/edit';
         $this->indexTemplet = 'Test/index';
-        $this->whereList = [];
+        $this->whereList = ['keywords' => 'like'];
     }
 }

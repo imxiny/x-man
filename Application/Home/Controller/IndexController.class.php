797f4aafbeec74354ba8a9aaf44da39d4783b9d8
xiny <yao_xinyu@outlook.com>
2018-09-12 17:02:19 +0800

    home

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -4,25 +4,11 @@ namespace Home\Controller;
 
 
 use Think\Controller;
-use Org\Cloudstorage;
-use OSS\Core\OssException;
-use Think\Upload\Driver\Upyun;
 
 class IndexController extends Controller
 {
-    /**
-     * @throws \OSS\Core\OssException
-     */
     public function index()
     {
-        $o = new Cloudstorage\Upyuss();
-        var_dump($o);
-        /*$config = C('ALIOSSCONF');
-        try{
-            $o = new Cloudstorage\Alioss($config['KEY'], $config['SECRET'], $config['ENDPOINT']);
-            $t = $o->upload($config['BUCKET'], 'a/a.ico',"./favicon.ico");
-        }catch (OssException $e){
-            var_dump($e->getMessage());
-        }*/
+        $this->display();
     }
 }
